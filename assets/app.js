62d7658c1c72134dd143c369eb714c43e4c5605c
kevin <kevin@afoundria.com>
2015-08-04 08:30:14 -0500

    add the countdown display and bind it to the duration widgets.

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,37 @@
 
+function updateCountdown(toggleContainer, value) {
+    // find the .countdown-container that has the same data-duration-type (session or break)
+    var durationType = toggleContainer.data('duration-type');
+    var ctDown = $('.countdown-container[data-duration-type="' + durationType + '"]').find('#countdown');
+    ctDown.text(value);
+}
+
+
 $(document).ready(function() {
 
   $('.duration-input').each(function() {
     var widget = $(this);
     var valueElt = widget.find('.input-value');
 
-    widget.find('.minus-button').click(function() {
+    widget.find('.minus-button').click(function(evt) {
         var value = parseInt(valueElt.text(), 10);
         if (value > 0) {
             value--;
             valueElt.text(value);
+
+            updateCountdown($(this).closest('.toggle-container'), value);
         }
+
+        evt.preventDefault();
     });
 
-    widget.find('.plus-button').click(function() {
+    widget.find('.plus-button').click(function(evt) {
         var value = parseInt(valueElt.text(), 10);
         value++;
         valueElt.text(value);
+
+        updateCountdown($(this).closest('.toggle-container'), value);
+        evt.preventDefault();
     });
   });
 });
