f104ffa5ca3476d29dff2aa655271c2994fd654b
Kevin <kboliver82@yahoo.com>
2015-08-07 16:40:10 -0500

    switch between session and breaks whenever the countdown hits 0

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ function updateCountdown(toggleContainer, countdownContainer, value) {
 
 function toTimestamp(val) {
     return {
-        mins: val,
+        mins: parseInt(val,10),
         secs: 0
     };
 }
@@ -36,26 +36,45 @@ function decrementTimestamp(timestamp) {
     return result;
 }
 
+function nextDurationType(durationType) {
+   if (durationType === 'session') {
+       return 'break';
+   } else {
+       return 'session';
+   }
+}
+
 $(document).ready(function() {
   var countdownContainer = $('.countdown-container');
   var countdownType = countdownContainer.find('#countdownType');
   var countdown = countdownContainer.find('#countdown');
+  var durationType = countdownContainer.data('duration-type');
   var timestamp = toTimestamp(countdown.text());
   var timerInterval;
+
+  var durations = {
+      break: 'Break',
+      session: 'Session'
+  };
 
   $('.duration-input').each(function() {
     var widget = $(this);
     var valueElt = widget.find('.input-value');
 
     widget.find('.minus-button').click(function(evt) {
-        clearInterval(timerInterval);
+        var toggleContainer = $(this).closest('.toggle-container');
+        var dType = toggleContainer.data('duration-type');
 
         var value = parseInt(valueElt.text(), 10);
         if (value > 0) {
             value--;
             valueElt.text(value);
 
-            updateCountdown($(this).closest('.toggle-container'), countdownContainer, value);
+            updateCountdown(toggleContainer, countdownContainer, value);
+        }
+
+        if (dType === durationType) {
+            clearInterval(timerInterval);
             timestamp = toTimestamp(countdown.text());
         }
 
@@ -63,14 +82,19 @@ $(document).ready(function() {
     });
 
     widget.find('.plus-button').click(function(evt) {
-        clearInterval(timerInterval);
+        var toggleContainer = $(this).closest('.toggle-container');
+        var dType = toggleContainer.data('duration-type');
 
         var value = parseInt(valueElt.text(), 10);
         value++;
         valueElt.text(value);
 
-        updateCountdown($(this).closest('.toggle-container'), countdownContainer, value);
-        timestamp = toTimestamp(countdown.text());
+        updateCountdown(toggleContainer, countdownContainer, value);
+        if (dType === durationType) {
+            clearInterval(timerInterval);
+            timestamp = toTimestamp(countdown.text());
+        }
+
         evt.preventDefault();
     });
   });
@@ -84,6 +108,15 @@ $(document).ready(function() {
       updateClock(timestamp);
   };
 
+  var switchDurationTypes = function() {
+      durationType = nextDurationType(durationType);
+      countdownContainer.data('duration-type', durationType);
+      countdownContainer.attr('data-duration-type', durationType);
+      countdownType.text(durations[durationType]);
+      var value = $('.duration-input [data-duration-type=' + durationType + '] .input-value').text();
+      timestamp = toTimestamp(value);
+  };
+
   countdownContainer.click(function() {
       if (timerInterval) {
           clearInterval(timerInterval);
@@ -95,7 +128,7 @@ $(document).ready(function() {
               doClockTick();
 
               if (timestamp.mins === 0 && timestamp.secs === 0) {
-                  clearInterval(timerInterval);
+                  switchDurationTypes();
               }
           }, 1000);
       }
